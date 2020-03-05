e47b881940ca3716d3ab02d72e28052e19b6ded5
Ian Tracy <ian.tracy@lairdconnect.com>
2020-03-05 11:23:44 -0500

    create signed based off origional

--- rs191/rs1_signed.js
+++ rs191/rs1_signed.js
@@ -2,18 +2,22 @@ function on_join(packet, deveui) {
   var join_message = { deveui: deveui, lora: packet, join: 1 };
   return join_message;
 }
+
 function on_uplink(packet, payload, deveui) {
   var up_message = {};
-  let signed_payload = new Int8Array(payload);
+
   // temp and rh data notification
   if (payload[0] == 0x01) {
+    signed_payload = new Int8Array(payload);
     up_message.options = payload[1];
     up_message.humidity = payload[2] / 100.0 + payload[3];
+    up_message.temp = payload[4] / 100.0 + payload[5];
     up_message.temp = signed_payload[4] / 100.0 + signed_payload[5];
     up_message.battery = batteryTable[payload[6]];
     up_message.alarms = (payload[7] << 8) | payload[8];
     up_message.backlog = (payload[9] << 8) | payload[10];
   }
+
   // fw version notification
   if (payload[0] == 0x07) {
     up_message.options = payload[1];
@@ -31,17 +35,23 @@ function on_uplink(packet, payload, deveui) {
         payload[10]
       ]).toUpperCase();
   }
+
   return { deveui: deveui, lora: packet, data: up_message };
 }
+
 function on_downlink(message) {
   var downlink = null;
+
   if (message.msgType == 1) {
     // generic request
     downlink = new Uint8Array([message.msgType, message.options]);
   }
+
   return downlink;
 }
+
 var batteryTable = [0, 5, 20, 40, 60, 80];
+
 function createHexString(arr) {
   var result = "";
   for (i in arr) {
